b1be66f182e8066ebd6b42f751be927828cd0fd8
Guangchuang Yu <guangchuangyu@gmail.com>
2014-06-23 11:41:27 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: d4030ce291d78f2b995540f1d0b736861c49f0f5
    Commit message: geneSim can only accept one gene ID vector and perform as mgeneSim in GOSemSim <2014-06-23, Mon>
    Committed by: Guangchuang Yu
    Author Name: Guangchuang Yu
    Commit date: 2014-06-23 19:41:01 +0800
    Author date: 2014-06-23 19:41:01 +0800
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DOSE@91736 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/geneSim.R
+++ R/geneSim.R
@@ -13,12 +13,18 @@
 ##' @export
 ##' @author Guangchuang Yu \url{http://ygc.name}
 geneSim <- function(geneID1,
-                    geneID2,
+                    geneID2=NULL,
                     measure="Wang",
                     combine="BMA") {
 
     DOID1 <- lapply(geneID1, gene2DO)
-    DOID2 <- lapply(geneID2, gene2DO)
+    if (is.null(geneID2)) {
+        geneID2 <- geneID1
+        DOID2 <- DOID1
+    } else {
+        DOID2 <- lapply(geneID2, gene2DO)
+    }
+    
     m <- length(geneID1)
     n <- length(geneID2)
     scores <- matrix(NA, nrow=m, ncol=n)
@@ -26,13 +32,24 @@ geneSim <- function(geneID1,
     colnames(scores) <- geneID2
 
     for (i in 1:m) {
-        for (j in 1:n) {
+        if (all(geneID1 == geneID2)) {
+           nn <- i
+           flag <- TRUE
+        } else {
+            flag <- FALSE
+            nn <- n
+        }
+        
+        for (j in 1:nn) {
             if(any(!is.na(DOID1[[i]])) &&  any(!is.na(DOID2[[j]]))) {
                 s <- doSim(DOID1[[i]],
                            DOID2[[j]],
                            measure
                            )
                 scores[i,j] = combineScores(s, combine)
+                if (flag == TRUE && j != i) {
+                    scores[j, i] <- scores[i,j]
+                }
             }
         }
     }
