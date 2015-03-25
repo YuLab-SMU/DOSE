f51252739817a9e35f68503a60c53c2362a1cf2e
g.yu <g.yu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-03-25 13:26:11 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: ebdd214b68859638210a87f35e1049e42cd1ea35
    
        update extdata
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DOSE@101146 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/extdata/buildAnnoData.R
+++ inst/extdata/buildAnnoData.R
@@ -1,9 +1,9 @@
-do.rif <- read.delim2("do_rif.human.txt", sep="\t",
+do.rif <- read.delim2(gzfile("do_rif.human.txt.gz"), sep="\t",
                       stringsAsFactors=F, header=F)
 eg.do1 <- do.rif[,c(1,5)]
 colnames(eg.do1) <- c("eg", "doid")
 
-domapping <- read.delim("IDMappings.txt", stringsAsFactors=F)
+domapping <- read.delim(gzfile("IDMappings.txt.gz"), stringsAsFactors=F)
 eg.do2 <- domapping[,c(2,1)]
 colnames(eg.do2) <- c("eg", "doid")
 eg.do2$doid <- paste("DOID:", eg.do2$doid, sep="")
