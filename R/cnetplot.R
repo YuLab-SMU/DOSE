8ed816cab75dfc568da29a84d81ed8ccb5616e4b
Guangchuang Yu <guangchuangyu@gmail.com>
2015-05-28 02:55:49 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 9ada660e10a2272cca3bbf2ea965ed92cfe2ea92
    
        Merge pull request #2 from vladpetyuk/master
    
        fix bin agreement
    
    Commit id: 51c7d10fd261d8456a0926c3d7c7be96090e5d8b
    
        follow-up fix of col.bin
    
    
    Commit id: 9e1fffc88a70f8def2110857e331eb346ad61cd9
    
        fix bin agreement
    
        number of breaks provided to hist(.) is take only as suggestion and may not match the resulting number of breaks
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DOSE@104298 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/cnetplot.R
+++ R/cnetplot.R
@@ -148,11 +148,11 @@ netplot <- function(g,
             lbs <- hist(fc, breaks=col.bin-1, plot=FALSE)$breaks
             col.legend <- get.col.scale(lbs)
 
-            x <- seq(from=legend.x, by=0.03, length.out=col.bin)
-            y <- rep(legend.y, col.bin)
+            x <- seq(from=legend.x, by=0.03, length.out=length(col.legend))
+            y <- rep(legend.y, length(col.legend))
             points(x, y, pch=15, col=col.legend, cex=2)
 
-            idx <- c(1, seq(4, col.bin-1, by=3), col.bin)
+            idx <- c(1, seq(4, length(col.legend)-1, by=3), length(col.legend))
             text(x=x[idx],
                  y=rep(legend.y-0.05, length(idx)),
                  label=lbs[idx],
