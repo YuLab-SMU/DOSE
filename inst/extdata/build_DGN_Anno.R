b16c76f94fdcbe80a74b05bbec538f50e2c14de1
GuangchuangYu <guangchuangyu@gmail.com>
2016-08-01 21:55:56 +0800

    support DisGeNET enrichment analyses

--- inst/extdata/build_DGN_Anno.R
+++ inst/extdata/build_DGN_Anno.R
@@ -5,7 +5,7 @@ d2g <- unique(x[, c(4, 1)])
 .DGN_DOSE_Env <- clusterProfiler:::build_Anno(d2g, d2n)
 
 
-save(.DGN_DOSE_Env, file="DGN_DOSE_Env.rda")
+save(.DGN_DOSE_Env, file="DGN_DOSE_Env.rda", compress='xz')
 
 
 
@@ -16,4 +16,4 @@ d2s <- unique(y[, c(5, 1)])
 
 .VDGN_DOSE_Env <- clusterProfiler:::build_Anno(d2s, d2n)
 
-save(.VDGN_DOSE_Env, file="VDGN_DOSE_Env.rda")
+save(.VDGN_DOSE_Env, file="VDGN_DOSE_Env.rda", compress='xz')
