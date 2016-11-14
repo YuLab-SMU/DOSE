ab168f2f9981961c5af56ac1b387da4cd90bc31f
Guangchuang Yu <guangchuangyu@gmail.com>
2016-11-14 09:45:24 +0000

    as.data.frame
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DOSE@123936 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/barplot.R
+++ R/barplot.R
@@ -59,21 +59,21 @@ fortify.enrichResult <- function(model, data, showCategory=5, by = "Count", orde
 ##'
 ##' @title theme_dose
 ##' @importFrom ggplot2 theme_bw
-##' @importFrom ggplot2 %+replace%
 ##' @importFrom ggplot2 theme
 ##' @importFrom ggplot2 element_text
+##' @importFrom ggplot2 margin
 ##' @export
 ##' @param font.size font size
 theme_dose <- function(font.size=14) {
-    theme_bw() %+replace%
+    theme_bw() +
     theme(axis.text.x = element_text(colour = "black",
-          size = font.size, vjust =1 ),
-          axis.title.x = element_text(colour="black",
-          size = font.size),
+                                     size = font.size, vjust =1 ),
           axis.text.y = element_text(colour = "black",
-          size = font.size, hjust =1 ),
-          axis.title.y = element_text(colour="black",
-          size = font.size, angle=90)
+                                     size = font.size, hjust =1 ),
+          axis.title = element_text(margin=margin(10, 5, 0, 0),
+                                    color = "black",
+                                    size = font.size),
+          axis.title.y = element_text(angle=90)
           )
 }
 
