edc11061ffb5d72bb15d1a02d22cc44f144cc2c8
GuangchuangYu <guangchuangyu@gmail.com>
2016-12-23 00:14:55 +0800

    dotplot for gseaResult

--- R/dotplot.R
+++ R/dotplot.R
@@ -6,10 +6,7 @@
 ##' @importFrom ggplot2 xlab
 ##' @importFrom ggplot2 ylab
 ##' @importFrom ggplot2 ggtitle
-dotplot.enrichResult <- function(object, x="geneRatio", colorBy="p.adjust", showCategory=10, split=NULL, font.size=12, title="") {
-    if (! is(object, "enrichResult")) {
-        stop("object should be an instance of 'enrichResult'...")
-    }
+dotplot_internal <- function(object, x="geneRatio", colorBy="p.adjust", showCategory=10, split=NULL, font.size=12, title="") {
     colorBy <- match.arg(colorBy, c("pvalue", "p.adjust", "qvalue"))
     if (x == "geneRatio" || x == "GeneRatio") {
         x <- "GeneRatio"
@@ -31,8 +28,3 @@ dotplot.enrichResult <- function(object, x="geneRatio", colorBy="p.adjust", show
             ylab("") + ggtitle(title) + theme_dose(font.size)
 }
 
-parse_ratio <- function(ratio) {
-    gsize <- as.numeric(sub("/\\d+$", "", as.character(ratio)))
-    gcsize <- as.numeric(sub("^\\d+/", "", as.character(ratio)))
-    return(gsize/gcsize)
-}
