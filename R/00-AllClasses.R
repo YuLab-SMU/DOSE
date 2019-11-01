54ef8b27084edba94b9b6da8d572f01aea2bfb15
Guangchuang Yu <guangchuangyu@gmail.com>
2019-11-01 23:33:48 +0800

    compareClusterResult class

--- R/00-AllClasses.R
+++ R/00-AllClasses.R
@@ -1,3 +1,30 @@
+##' Class "compareClusterResult"
+##' This class represents the comparison result of gene clusters by GO
+##' categories at specific level or GO enrichment analysis.
+##'
+##'
+##' @name compareClusterResult-class
+##' @aliases compareClusterResult-class show,compareClusterResult-method
+##'   summary,compareClusterResult-method plot,compareClusterResult-method
+##' @docType class
+##' @slot compareClusterResult cluster comparing result
+##' @slot geneClusters a list of genes
+##' @slot fun one of groupGO, enrichGO and enrichKEGG
+##' @slot .call function call
+##' @exportClass compareClusterResult
+##' @author Guangchuang Yu \url{https://guangchuangyu.github.io}
+##' @exportClass compareClusterResult
+##' @seealso 
+##'   \code{\linkS4class{enrichResult}}
+##' @keywords classes
+setClass("compareClusterResult",
+         representation = representation(
+             compareClusterResult = "data.frame",
+             geneClusters = "list",
+             fun = "character",
+             .call = "call"
+         )
+         )
 
 ##' Class "enrichResult"
 ##' This class represents the result of enrichment analysis.
