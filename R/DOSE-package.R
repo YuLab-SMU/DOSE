06e71b18bf00b5c40d928b7b440a6419388f4bca
Guangchuang Yu <guangchuangyu@gmail.com>
2024-02-06 00:14:23 +0800

    use _PACKAGE to document package manpage

--- R/DOSE-package.R
+++ R/DOSE-package.R
@@ -1,24 +1,5 @@
-#' Disease Ontology Semantic and Enrichment analysis
-#' Implemented five methods proposed by Resnik, Schlicker, Jiang, Lin and Wang respectively for measuring DO semantic similarities, and hypergeometric test for enrichment analysis.
-#'
-#' This package is designed to estimate DO-based semantic similarity
-#' measurement and enrichment analysis.
-#'
-#' \tabular{ll}{ Package: \tab DOSE\cr Type: \tab Package\cr Version: \tab
-#' 2.3.5\cr Date: \tab 2-27-2012\cr biocViews:\tab Bioinformatics,
-#' Annotation\cr Depends:\tab \cr Imports: \tab methods, AnnotationDbi,
-#' HDO.dbb\cr Suggests:\tab clusterProfiler, GOSemSim\cr License: \tab
-#' Artistic-2.0\cr }
-#'
-#' @name DOSE-package
-#' @aliases DOSE-package DOSE
-#' @docType package
-#' @author Guangchuang Yu, Li-Gen Wang
-#'
-#' Maintainer: Guangchuang Yu \email{guangchuangyu@@gmail.com}
-#' @seealso \linkS4class{enrichResult}
-#' @keywords package
-NULL
+#' @keywords internal
+"_PACKAGE"
 
 
 #' Datasets
