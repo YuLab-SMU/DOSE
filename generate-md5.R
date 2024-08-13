ad38261b57a9916d341e41869c5179d66d89b23e
Guangchuang Yu <guangchuangyu@gmail.com>
2024-08-13 16:41:36 +0800

    update md5

--- generate-md5.R
+++ generate-md5.R
@@ -1,4 +1,4 @@
 ff <- list.files(pattern = ".sqlite$")
-md5 <- vapply(ff, function(f) digest::digest(f, algo='md5'), character(1))
+md5 <- vapply(ff, function(f) digest::digest(f, algo='md5', file=TRUE), character(1))
 cat(sprintf("%s\t%s\n", ff, md5), file="md5.txt", sep="")
 
